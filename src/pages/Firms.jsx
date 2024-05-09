4145bf078a727270523f03514cb4cd4146a39a81
busra <kocarslanbusrab@gmail.com>
2024-05-09 20:06:07 +0300

    loading

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 import useStockRequest from "../services/useStockRequest";
 import { useDispatch, useSelector } from "react-redux";
 import FirmsCard from "../component/FirmsCard";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Modal from "../component/Modal";
 import FirmsModal from "../component/Modal";
+import loading from "../assets/loading.gif"
 // import { firmPending } from "../features/firmSlice"
 
 const Firms = () => {
@@ -21,8 +22,9 @@ const Firms = () => {
     <Box>
       <h2>Firms</h2>
       <FirmsModal/>
-      {/* <loading && <img src/> */}
-      {/* error && <Typografy>hay aksi</Typografy> */}
+      {loading && <img src={loading} alt="Loading" />} 
+    
+      {error && <Typography variant="body1">Hay aksi, bir hata oluştu!</Typography>}
 
       <Box display="flex" flexWrap="wrap" justifyContent="space-between" alignItems="center">
         {firmsData?.map((firm) => (
